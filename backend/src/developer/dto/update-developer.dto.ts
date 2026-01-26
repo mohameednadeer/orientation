2b8d9b930124645e7abb09c88cb022743af303e6
mohmedlotfy <mohmedlotfy2003@gmail.com>
2026-01-27 01:48:59 +0200

    Flutter project - main branch with Flutter files only

--- backend/src/developer/dto/update-developer.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateDeveloperDto } from './create-developer.dto';
-
-/**
- * UpdateDeveloperDto - Allows updating developer data except projects
- * Note: Projects field is not included in CreateDeveloperDto, so it cannot be updated here.
- * Projects are managed separately through the Projects module.
- */
-export class UpdateDeveloperDto extends PartialType(CreateDeveloperDto) {}
