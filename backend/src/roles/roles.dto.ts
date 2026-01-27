21a03fcbd24883c5bfc2c1184aac76f15aacc6eb
mohmedlotfy <mohmedlotfy2003@gmail.com>
2026-01-27 04:55:30 +0200

    Remove backend folder

--- backend/src/roles/roles.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IsEnum } from 'class-validator';
-
-export class RoleUserDto {
-  @IsEnum(['admin', 'user', 'developer', 'superadmin'])
-  role: 'admin' | 'user' | 'developer' | 'superadmin';
-}
